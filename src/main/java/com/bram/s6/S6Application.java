9b407bdf36860a74aa0b40493d58c1a57b0b50b8
Bramcoen <coenders123@gmail.com>
2022-05-17 15:37:26 +0200

    Add route

--- src/main/java/com/bram/s6/S6Application.java
+++ src/main/java/com/bram/s6/S6Application.java
@@ -2,6 +2,9 @@ package com.bram.s6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class S6Application {
@@ -10,4 +13,11 @@ public class S6Application {
 		SpringApplication.run(S6Application.class, args);
 	}
 
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+		return builder.routes().
+				route("messaging", r->  r.path("/messaging/*")
+						.uri("https://messaging:443"))
+				.build();
+	}
 }
