b7dc2e727670c8f057526f5c0bdb332311149f98
Bramcoen <coenders123@gmail.com>
2022-05-17 12:59:05 +0200

    Application files spring initializer

---
+++ src/test/java/com/bram/s6/S6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bram.s6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
